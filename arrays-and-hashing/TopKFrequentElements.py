e34b2a1057a08e066ae2ba660ed50483b27c7eb2
Ameen Durani <uni.ameend@gmail.com>
2026-01-12 19:25:05 -0500

    Top K Frequent elements!

---
+++ arrays-and-hashing/TopKFrequentElements.py
@@ -0,0 +1,24 @@
+# Top K Frequent Elements
+# https://neetcode.io/problems/top-k-elements-in-list/question
+
+# Complexity
+# - Time: O(n)
+# - Space: O(n)
+
+# Code
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        res = [[] for i in range(len(nums) + 1)] # +1 since we can have exactly the lenght of nums of frequency
+
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        for key, value in count.items():
+            res[value].append(key)
+        
+        out = []
+        for i in range(len(res) - 1, 0, -1): # -1 since we don't check 0 frequency numbers
+            for num in res[i]:
+                out.append(num)
+                if len(out) == k:
+                    return out
